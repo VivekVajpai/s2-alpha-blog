c80a53183026ccfca1b467fc73f5b1549a496ba9
Vivek Vajpai <vivekvajpai65@gmail.com>
2021-11-15 17:33:39 +0530

    Add create new article facility

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,14 @@ class ArticlesController < ApplicationController
 		@articles = Article.all
 	end
 
+	def new
+		# @articles = Article.new
+	end
+
+	def create
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		@article.save
+		redirect_to @article
+	end
+
 end
